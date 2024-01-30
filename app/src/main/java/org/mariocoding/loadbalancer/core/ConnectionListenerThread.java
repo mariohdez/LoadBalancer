7349b4e93ff8e351febe8689504915973453fd42
Mario Hernandez <mhmario963@gmail.com>
2024-01-29 21:59:13 -0500

    Revert "Delete app directory"
    This reverts commit 091e171ae1277aaa032d27f9c16f7bcd4b5f8ee4.

---
+++ app/src/main/java/org/mariocoding/loadbalancer/core/ConnectionListenerThread.java
@@ -0,0 +1,61 @@
+package org.mariocoding.loadbalancer.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ConnectionListenerThread extends Thread implements Closeable {
+	private final static Logger LOGGER = LoggerFactory.getLogger(ConnectionListenerThread.class);
+
+	private final int port;
+	private final ServerSocket serverSocket;
+
+	public ConnectionListenerThread(int port) throws IOException {
+		this.port = port;
+		this.serverSocket = new ServerSocket(this.port);
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (this.serverSocket.isBound() && !this.serverSocket.isClosed()) {
+                LOGGER.info("Server Socket is waiting for connection...");
+				Socket socket = this.serverSocket.accept();
+                LOGGER.info("Server Socket connection accepted."); 
+				StringBuilder strBuilder = new StringBuilder();
+				strBuilder.append(socket.getLocalAddress() + " ");
+				strBuilder.append(socket.getLocalPort() + " ");
+				strBuilder.append(socket.getInetAddress() + " ");
+				strBuilder.append(socket.getPort());
+
+                LOGGER.info("socket unique identifier: {}", strBuilder.toString());
+
+                socket.getOutputStream().write("".getBytes());
+                LOGGER.info("Server Socket closing.");
+			}
+		}  catch (IOException ioe) {
+			LOGGER.error("Problem with setting socket", ioe);
+		} finally {
+			try {
+				this.serverSocket.close();
+			} catch (IOException ioe) {
+			}
+		}
+	}
+
+	@Override
+	public void close() {
+		if (this.serverSocket != null) {
+			try {
+				this.serverSocket.close();
+			} catch (IOException ioe) {
+				LOGGER.error("Could not properly dispose of server socket.");
+			}
+		}
+	}
+}
+
