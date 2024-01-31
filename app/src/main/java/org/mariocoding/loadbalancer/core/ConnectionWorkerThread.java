58bf14916f57506c306ae8cec234d0ba74bc4846
Mario Hernandez <mhmario963@gmail.com>
2024-01-30 23:12:05 -0500

    add extends thread to connection worker.

--- app/src/main/java/org/mariocoding/loadbalancer/core/ConnectionWorkerThread.java
+++ app/src/main/java/org/mariocoding/loadbalancer/core/ConnectionWorkerThread.java
@@ -4,11 +4,16 @@ import java.net.Socket;
 
 import org.mariocoding.loadbalancer.model.Server;
 
-public class ConnectionWorkerThread {
+public class ConnectionWorkerThread extends Thread {
     private final Socket clientSocket;
     private Socket serverSocket;
 
     public ConnectionWorkerThread(Socket socket, Server server) {
         this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        // TODO: implement server socket connection and replying back to client with response.
     }
 }
